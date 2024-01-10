6abaeda5b3cae4834d1e3743758937a99815b3e5
Masayuki Akamatsu <aka@iamas.ac.jp>
2024-01-11 06:36:45 +0900

    Initial Commit

---
+++ WindowStyleTest/WindowStyleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowStyleTestApp.swift
+//  WindowStyleTest
+//
+//  Created by Masayuki Akamatsu on 2024/01/11.
+//
+
+import SwiftUI
+
+@main
+struct WindowStyleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
